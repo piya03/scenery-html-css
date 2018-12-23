c0f3939cc7e93f4d285c0923e1fe9d06757599ad
piy03 <piyachaudhary50@gmail.com>
2018-12-23 21:59:01 +0530

    fix

---
+++ main.js
@@ -0,0 +1,6 @@
+// $(document).mousemove(function(e){
+//     $('.pointer').css({left:e.pageX, top:e.pageY})
+//   })
+// $(document).mousemove(function(e){
+//       $('.butterfly1').css({left:e.pageX, top:e.pageY})
+// })
