17c09f9b22ce548e99d2e79a63809bdf7eee3b98
piya03 <piyachaudhary50@gmail.com>
2018-12-25 23:26:05 +0530

    fixes

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 // $(document).mousemove(function(e){
 //     $('.pointer').css({left:e.pageX, top:e.pageY})
 //   })
-// $(document).mousemove(function(e){
-//       $('.butterfly1').css({left:e.pageX, top:e.pageY})
-// })
+$(".container").mouseover(function(e){
+    console.log("e.pageX", e.pageX, e.pageY)
+      $('.butterfly1').css({left:`${e.pageX}px`, top:`${e.pageY}px`})
+})
+
+const container = document.getElementsByClassName("container")[0]
+container.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+    console.log("e", e.touches)
+    var touch = e.touches[0];
+    //alert(touch.pageX + " - " + touch.pageY);
+    $('.butterfly1').css({left:`${touch.pageX}px`, top:`${touch.pageY}px`})
+    $('.butterfly-2').css({left:`${touch.pageX + 20}px`, top:`${touch.pageY - 10}px`})
+}, false);
